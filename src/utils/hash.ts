a522389af7726a732fe4ac79a0defc063a343c31
MarwanTarik <marwantarik64@gmail.com>
2024-02-16 00:45:46 +0200

    hash utility added

---
+++ src/utils/hash.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+import stageConfig from '../configs/main'
+
+async function comparePassword (password: string, hashedPassword: string): Promise<boolean> {
+  return await bcrypt.compare(password, hashedPassword)
+}
+
+async function hashPassword (password: string): Promise<string> {
+  return await bcrypt.hash(password, stageConfig.SALT as string)
+}
+
+export {
+  comparePassword,
+  hashPassword
+}
